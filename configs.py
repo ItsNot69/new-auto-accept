013df4889eb9ae43e68576319fbb310c2ed472f9
ItsNot69 <126782133+ItsNot69@users.noreply.github.com>
2024-03-11 00:57:58 -0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ class Config:
     API_ID = int(getenv("API_ID", "28726816"))
     API_HASH = getenv("API_HASH", "45edf74203ecf6ff14b394a9e47dca34")
     BOT_TOKEN = getenv("BOT_TOKEN", "6891263386:AAGdH44TNWeShyBn0qma1DEcpXBAAGksl9A")
-    FSUB = getenv("FSUB", "born4movies7")
-    CHID = int(getenv("CHID", "-1001502035253"))
+    FSUB = getenv("FSUB", "link_hub_desi_hub")
+    CHID = int(getenv("CHID", "-1002109589383"))
     SUDO = list(map(int, getenv("SUDO", "5521380948").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://m0921594:Rohit44@cluster0.qbze22i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
